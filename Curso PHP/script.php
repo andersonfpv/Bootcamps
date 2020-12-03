7ecf881003d110186a13134bca954391bee4c10c
Anderson Felipe <anderson_fpv@yahoo.com.br>
2020-12-03 18:43:22 -0300

    Adicionando validação de campos do formulário

--- Curso PHP/script.php
+++ Curso PHP/script.php
@@ -13,6 +13,26 @@ $idade = $_POST['idade'];
 
 //var_dump($nome);
 //var_dump($idade);
+if (empty($nome)){
+	echo 'O nome não pode ser vazio';
+	return;
+}
+
+if(strlen($nome) < 3){
+	echo 'O nome deve conter mais de 3 caracteres';
+	return;
+}
+
+if(strlen($nome) > 40){
+	echo 'O nome é muito extenso';
+	return;
+}
+
+if(!is_numeric($idade)){
+	echo "Informe um número para a idade";
+	return;
+}
+
 
 if($idade >= 6 && $idade <= 12){
 
